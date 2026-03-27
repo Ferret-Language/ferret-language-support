e457fe47e9dd6209f4e8d44a7a0c2f0f1e32af7b
Fuad Hasan <fuad.cs22@gmail.com>
2026-03-28 01:31:01 +0600

    Move/rename Ferret SVGs, update icons and indents
    Move ferret SVG assets into their language folders and update configs
    to reference the new filenames. Extend Ferret indentation rules with:
    parameter_list, argument_list, type_parameter_list, type_argument_list,
    array_literal, bracket_composite_literal, and tuple_type

--- languages/ferret/indents.scm
+++ languages/ferret/indents.scm
@@ -1,12 +1,19 @@
 [
   (block)
+  (parameter_list)
+  (argument_list)
+  (type_parameter_list)
+  (type_argument_list)
   (struct_type)
   (interface_type)
   (enum_type)
   (union_type)
   (error_type)
   (match_expression)
+  (array_literal)
+  (bracket_composite_literal)
   (composite_literal)
+  (tuple_type)
 ] @indent
 
 ["}" "]" ")"] @outdent
